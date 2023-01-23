7f1d6a10fdd25b321fb2a1d6f0829e864f504a2a
Israel Campos Ruiz <israel.campos@ugd.gob.mx>
2023-01-22 19:09:12 -0600

    failed deploy

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,4 +6,10 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  it('should have react native text', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/should have react native text/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>this work? vercel rules with github actions</p>
+        <p>this work? vercel rules with github actions?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
