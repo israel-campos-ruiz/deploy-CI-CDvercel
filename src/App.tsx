a67bbcd3cafea324c256195ac71bdc2c61b8cea7
Israel Campos Ruiz <israel.campos@ugd.gob.mx>
2023-01-22 18:57:07 -0600

    add text

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>this work? vercel rules with github actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
