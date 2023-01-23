11ee453f7c9eb82f923905c2e45b097e8b9c8575
Israel Campos Ruiz <israel.campos@ugd.gob.mx>
2023-01-22 19:17:02 -0600

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,10 +6,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-
-  it('should have react native text', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/should have react native text/i);
-    expect(linkElement).toBeInTheDocument();
-  });
 });
